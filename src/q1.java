3fb997d4b2fd50c8293b620e91053f8dc1777ec9
Aamir Jamil <digiart.aamir@gmail.com>
2024-05-15 00:03:53 +0500

    test

---
+++ src/q1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+
+public class q1 {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter marks of subject 1");
+        int m1= sc.nextInt();
+
+        System.out.println("Enter marks of subject 2");
+        int m2= sc.nextInt();
+
+        System.out.println("Enter marks of subject 3");
+        int m3= sc.nextInt();
+
+        System.out.println("Enter marks of subject 4");
+        int m4= sc.nextInt();
+
+        System.out.println("Enter marks of subject 5");
+        int m5= sc.nextInt();
+
+        int sum = m1+m2+m3+m4+m5;
+        int avg= sum/5;
+
+        System.out.print("sum of marks are: ");
+        System.out.println(sum);
+        System.out.print("average of marks are: ");
+        System.out.println(avg);
+
+
+
+    }
+}
